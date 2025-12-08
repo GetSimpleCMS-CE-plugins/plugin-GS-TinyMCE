8e87f16089efd187546e7f546d8a2a3b1461f9ee
risingisland <risingisland@yahoo.com>
2025-12-08 10:32:13 +0000

    update

--- GS-TinyMCE.php
+++ GS-TinyMCE.php
@@ -12,7 +12,7 @@ register_plugin(
 	'1.0',
 	'CE Team',
 	'https://www.getsimple-ce.ovh/ce-plugins',
-	'Replace the admin CKEditor with TinyMCE.',
+	'Replace Pages CKEditor with TinyMCE.',
 	'plugins',
 	'gs_tinymce'
 );
@@ -26,8 +26,9 @@ $GS_TINYMCE_SETTINGS = $GS_TINYMCE_DATA_DIR . '/gs-tinymce.json';
 
 $default_settings = array(
 	'selector' => '#post-body, #post-content, #content, #codetext',
-	'plugins'  => 'link image code lists paste table wordcount',
-	'toolbar'  => 'undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image media| table | code',
+	'sections' => 'edit.php',
+	'plugins'  => 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table wordcount',
+	'toolbar'  => 'undo redo | blocks | bold italic underline strikethrough removeformat | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image hr charmap | fullscreen code preview',
 	'menubar'  => true,
 	'relative_urls' => false,
 	'use_cdn'  => false,
@@ -78,9 +79,17 @@ function gs_tinymce_get_languages($version){
 add_action('common', 'gs_tinymce_detect_edit_page');
 
 function gs_tinymce_detect_edit_page() {
-	if (basename($_SERVER['PHP_SELF']) == 'edit.php') {
-		add_action('footer', 'gs_tinymce_header');
-	}
+    $settings = gs_tinymce_load_settings();
+
+    // explode user-defined sections
+    $sections = array_filter(array_map('trim', explode(',', $settings['sections'])));
+
+    $current = basename($_SERVER['PHP_SELF']);
+
+    // if current admin page is listed, load TinyMCE
+    if (in_array($current, $sections)) {
+        add_action('footer', 'gs_tinymce_header');
+    }
 }
 
 function gs_tinymce_header(){
@@ -198,7 +207,10 @@ function gs_tinymce_header(){
 			array('title' => 'Same window', 'value' => '_self')
 		),
 		'extended_valid_elements' => 'img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|style],a[href|target|title|class|rel]',
-		'link_list' => '__GS_LINK_LIST__'  // Placeholder to be replaced
+		'link_list' => '__GS_LINK_LIST__',  // Placeholder to be replaced
+		'quickbars_selection_toolbar' => 'bold italic underline| blocks | quicklink',
+		'quickbars_insert_toolbar' => 'quickimage quicktable | hr pagebreak',
+		'quickbars_image_toolbar' => 'alignleft aligncenter alignright | imageoptions'
 	);
 
 	// Ensure SITEURL has trailing slash for TinyMCE
@@ -360,6 +372,7 @@ function gs_tinymce(){
 		$new['autoresize'] = isset($_POST['autoresize']);
 		$new['autoresize_bottom_margin'] = (int)$_POST['autoresize_bottom_margin'];
 		$new['toolbar_mode'] = trim($_POST['toolbar_mode']);
+		$new['sections'] = trim($_POST['sections']);
 
 		gs_tinymce_save_settings($new);
 		$settings = $new;
@@ -367,17 +380,20 @@ function gs_tinymce(){
 
 		echo '<div class="updated">Settings saved.</div>';
 	}
-
+	
+	global $USR;
+	
 	echo '
 <style>
-	table tr,td {border-bottom: 0!important; border-top: 0!important;}
-	.w3-parent hr {important;margin: 10px 0!important;}
+	table tr,td {border-bottom: 0!important; border-top: 0!important;} .w3-btn{margin-right: 5px !important;}
+	.w3-parent hr {important;margin: 10px 0!important;} .hide{display:none;}
 </style>';
 
 	echo '
 <div class="w3-parent">
 	<header class="w3-container w3-border-bottom w3-margin-bottom">
-		<h3>GS-TinyMCE Settings</h3>
+		<h3>GS-TinyMCE Settings <svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle" width="1.5em" height="1.5em" viewBox="0 0 512 512"><rect width="512" height="512" fill="none"/><path fill="#335DFF" d="m512 221.768l-16.778-16.976l-34.794 34.389L259.251 38.956L0 295.246l16.78 16.975l39.813-39.36l201.169 200.183zM259.195 72.574l184.258 183.384L257.82 439.43L73.567 256.082zm61.412 120.453H196.1v-21.004h124.508zm38.514 51.534H157.585v-21.003H359.12zm0 50.785H157.585v-21.003H359.12zm-39.066 50.785H196.651v-21.003h123.404z"/></svg></h3>
+		<p>Replace Pages CKEditor with TinyMCE.</p>
 	</header>
 
 	<form method="post">
@@ -392,6 +408,8 @@ function gs_tinymce(){
 					
 					<button type="button" class="w3-btn w3-round-large w3-indigo" onclick="tinymcePreset(\'full\')">Advanced</button>
 					
+					<button type="button" class="hide w3-btn w3-round-large w3-deep-purple" onclick="tinymcePreset(\'expert\')">Expert <small>(v7+)</small></button>
+					
 					<br><small>Click to auto-fill the Plugins and Toolbar fields.</small>
 				</td>
 			</tr>
@@ -403,17 +421,26 @@ function tinymcePreset(type){
 
 	if(type === \'basic\'){
 		pluginsField.value = "link image lists";
-		toolbarField.value = "bold italic | bullist numlist | link image";
+		
+		toolbarField.value = "undo redo | blocks bold italic | bullist numlist | alignleft aligncenter alignright alignjustify | link image hr";
 	}
 
 	if(type === \'classic\'){
-		pluginsField.value = "link image code lists paste table wordcount";
-		toolbarField.value = "undo redo | formatselect | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image | table | code";
+		pluginsField.value = "advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table wordcount";
+		
+		toolbarField.value = "undo redo | blocks | bold italic underline strikethrough removeformat | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image hr charmap | fullscreen code preview";
 	}
 
 	if(type === \'full\'){
-		pluginsField.value = "anchor autolink charmap codesample image link lists media searchreplace table visualblocks wordcount code fullscreen";
-		toolbarField.value = "undo redo | blocks | bold italic underline strikethrough | link image media table | align | bullist numlist | outdent indent | charmap codesample | fullscreen code";
+		pluginsField.value = "preview searchreplace autolink code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount charmap quickbars accordion";
+		
+		toolbarField.value = "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough removeformat | forecolor backcolor | link image media | align numlist bullist | accordion accordionremove | lineheight outdent indent | hr pagebreak anchor | table tabledelete | charmap code fullscreen | print preview";
+	}
+
+	if(type === \'expert\'){
+		pluginsField.value = "tableofcontents powerpaste formatpainter footnotes export editimage checklist casechange advtable a11ychecker preview searchreplace autolink advcode visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount charmap quickbars accordion";
+		
+		toolbarField.value = "a11ycheck | undo redo | blocks fontfamily fontsize | bold italic underline strikethrough removeformat casechange | link image media | hr pagebreak anchor | align numlist bullist checklist | table tabledelete tableofcontents footnotes | accordion accordionremove | outdent indent lineheight| formatpainter forecolor backcolor | charmap code | fullscreen print export preview";
 	}
 }
 </script>
@@ -494,19 +521,28 @@ function tinymcePreset(type){
 				</td>
 			</tr>
 
+			<tr class="hide"><td colspan="2"><hr></td></tr>
+			
+			<tr class="hide">
+				<th>Sections</th>
+				<td>
+					<input type="text" name="sections" value="' . htmlspecialchars($settings['sections']) . '" style="width:95%"/>
+				</td>
+			</tr>
+			
+			<tr class="hide">
+				<th>Selector</th>
+				<td>
+					<input type="text" name="selector" value="' . htmlspecialchars($settings['selector']) . '" style="width:95%"/>
+				</td>
+			</tr>
+			
 			<tr><td colspan="2"><hr></td></tr>
 
 			<tr>
 				<th>TinyMCE Version</th>
 				<td>
 					<input type="number" name="tinymce_version" value="' . htmlspecialchars($settings['tinymce_version']) . '" style="width:20%"/>
-				</td>
-			</tr>
-
-			<tr>
-				<th>Selector</th>
-				<td>
-					<input type="text" name="selector" value="' . htmlspecialchars($settings['selector']) . '" style="width:95%"/>
 				</td>
 			</tr>
 
@@ -532,6 +568,51 @@ function tinymcePreset(type){
 
 		<p><input type="submit" name="gs_tinymce_save" class="w3-btn w3-round-large w3-green" value="Save settings"/></p>
 	</form>
+	
+	<div class="w3-padding-top-32 margin-bottom-none w3-border-top">
+		<p style="margin:0 0 5px 0"><svg xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle" width="1em" height="1em" viewBox="0 0 48 48"><rect width="48" height="48" fill="none"/><circle cx="24" cy="24" r="21" fill="#2196f3"/><path fill="#fff" d="M22 22h4v11h-4z"/><circle cx="24" cy="16.5" r="2.5" fill="#fff"/></svg> <b>About: </b></p>
+		<ul>
+			<li>Built with TinyMCE 6.8.6 (MIT license).</li>
+			<li>Full v6 <a href="https://www.tiny.cloud/docs/tinymce/6/plugins/#open-source-plugins" target="_blank">Plugin</a> & <a href="https://www.tiny.cloud/docs/tinymce/6/available-toolbar-buttons/" target="_blank">Toolbar</a> documentation.</li>
+			<li>v7+ both local and CDN versions require an <a href="https://www.tiny.cloud/tinymce/" target="_blank">API key</a>, but offer <a href="https://www.tiny.cloud/tinymce/features/" target="_blank">advanced features</a>.</li>
+		</ul>
+	</div>
+	
+	<footer class="w3-padding-top-32 margin-bottom-none w3-border-top">
+			<p class="w3-small clear w3-margin-bottom w3-margin-left">Made with 
+				<span class="credit-icon">❤️</span> especially for "
+				<b>'.$USR.'</b>". Is this plugin useful to you?
+	
+				<span class="w3-btn w3-khaki w3-border w3-border-red w3-round-xlarge">
+					<a href="https://getsimple-ce.ovh/donate" target="_blank" class="donateButton">
+						<b>Buy Us A Coffee </b>
+						<svg
+							xmlns="http://www.w3.org/2000/svg" style="vertical-align:middle" width="24" height="24" viewBox="0 0 24 24">
+							<path fill="currentColor" fill-opacity="0" d="M17 14v4c0 1.66 -1.34 3 -3 3h-6c-1.66 0 -3 -1.34 -3 -3v-4Z">
+								<animate fill="freeze" attributeName="fill-opacity" begin="0.8s" dur="0.5s" values="0;1"/>
+							</path>
+							<g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+								<path stroke-dasharray="48" stroke-dashoffset="48" d="M17 9v9c0 1.66 -1.34 3 -3 3h-6c-1.66 0 -3 -1.34 -3 -3v-9Z">
+									<animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="48;0"/>
+								</path>
+								<path stroke-dasharray="14" stroke-dashoffset="14" d="M17 9h3c0.55 0 1 0.45 1 1v3c0 0.55 -0.45 1 -1 1h-3">
+									<animate fill="freeze" attributeName="stroke-dashoffset" begin="0.6s" dur="0.2s" values="14;0"/>
+								</path>
+								<mask id="lineMdCoffeeHalfEmptyFilledLoop0">
+									<path stroke="#fff" d="M8 0c0 2-2 2-2 4s2 2 2 4-2 2-2 4 2 2 2 4M12 0c0 2-2 2-2 4s2 2 2 4-2 2-2 4 2 2 2 4M16 0c0 2-2 2-2 4s2 2 2 4-2 2-2 4 2 2 2 4">
+										<animateMotion calcMode="linear" dur="3s" path="M0 0v-8" repeatCount="indefinite"/>
+									</path>
+								</mask>
+								<rect width="24" height="0" y="7" fill="currentColor" mask="url(#lineMdCoffeeHalfEmptyFilledLoop0)">
+									<animate fill="freeze" attributeName="y" begin="0.8s" dur="0.6s" values="7;2"/>
+									<animate fill="freeze" attributeName="height" begin="0.8s" dur="0.6s" values="0;5"/>
+								</rect>
+							</g>
+						</svg>
+					</a>
+				</span>
+			</p>
+		</footer>
 </div>';
 
 }
